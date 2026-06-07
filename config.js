5eee4041b87dccd72a6770b7766118ba542734ea
tuanai2024-sg <tuanai2024@gmail.com>
2026-06-07 11:15:18 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.json({
+    apiKey:      process.env.FIREBASE_API_KEY,
+    authDomain:  process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId:   process.env.FIREBASE_PROJECT_ID,
+  });
+}
